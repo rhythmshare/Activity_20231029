3622f0aae0d0058fcd47d1a91af41a52979b5535
rhythmshare <randomcony@gmail.com>
2023-10-29 09:55:30 +0900

    DataBinding SecondActivity

--- app/src/main/java/com/example/activity_20231029/SecondActivity.kt
+++ app/src/main/java/com/example/activity_20231029/SecondActivity.kt
@@ -2,10 +2,13 @@ package com.example.activity_20231029
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.activity_20231029.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_second)
     }
 }
