fc0d74c2a47bdd120998612eb5c368698b637d70
rhythmshare <randomcony@gmail.com>
2023-10-29 09:51:26 +0900

    Use Intent (Move to SecondActivity)

--- app/src/main/java/com/example/activity_20231029/MainActivity.kt
+++ app/src/main/java/com/example/activity_20231029/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.activity_20231029
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -10,5 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//         버튼이 눌리면 = > SecondActivity화면으로 이동
+        binding.btnMoveToSecond.setOnClickListener {
+
+//        화면 이동시키기 : Intent 클래스 활용
+//        1. 어디서 출발해서 어디로 도착할지 정보 명시 (티켓)
+            val myIntent = Intent(this, SecondActivity::class.java)
+
+//        2. myIntent에 적힌대로 실제 화면 이동
+            startActivity(myIntent)
+        }
     }
 }
